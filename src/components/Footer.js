5cf959d5310f12867b30b24e8c5e64308ab6bf6a
William Hill <mjh2225@gmail.com>
2019-01-21 13:34:39 -0800

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Footer = () => (
+  <footer className="footer" id="footer" style={{backgroundColor: '#f5f5f5', padding: '1rem 1.5rem 1rem'}}>
+    <p>Designed by <a href="mjhill.net">M.J. Hill Digital</a></p>
+  </footer>
+);
+export default Footer;

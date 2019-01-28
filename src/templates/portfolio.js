cceef21615508c7cf6248eb878a24c84bbeeb4fa
William Hill <mjh2225@gmail.com>
2019-01-27 22:29:03 -0800

    cleanup

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,32 +1,18 @@
 import React, { Component } from 'react'
 import Img from 'gatsby-image'
+import Layout from '../components/Layout'
 import Lightbox from 'react-images'
-import Gallery from 'react-photo-gallery';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 class GalleryComposition extends Component {
   constructor(props) {
     super(props)
-    console.log("props:", this.props)
     const data = props.data
     const photos = data.allFile.edges
-    console.log("photos:", photos)
     const LIGHTBOX_IMAGE_SET = []
     photos.map(photo => {
-      var fraction = photo.node.childImageSharp.fluid.aspectRatio
-      const len = fraction.toString().length - 2;
-      var denominator = Math.pow(10, len);
-      var numerator = fraction * denominator;
-      var divisor = this.gcd(numerator, denominator);    // Should be 5
-
-      numerator /= divisor;                         // Should be 687
-      denominator /= divisor;                       // Should be 2000
-      // console.log(Math.floor(numerator) + '/' + Math.floor(denominator));
-      const width = Math.floor(numerator)
-      const height = Math.floor(denominator)
-      LIGHTBOX_IMAGE_SET.push({src: photo.node.childImageSharp.fluid.src, thumbnail: photo.node.childImageSharp.fluid.src,  srcSet: photo.node.childImageSharp.fluid.srcSet, width: width, height:height })
+      LIGHTBOX_IMAGE_SET.push({src: photo.node.childImageSharp.fluid.src, thumbnail: photo.node.childImageSharp.fluid.src,  srcSet: photo.node.childImageSharp.fluid.srcSet })
     })
-    console.log("LIGHTBOX_IMAGE_SET:", LIGHTBOX_IMAGE_SET)
     this.state = {
       shareOpen: false,
       anchorEl: null,
@@ -36,12 +22,6 @@ class GalleryComposition extends Component {
       photos_fluid: photos
     }
   }
-
-  gcd(a, b) {
-    if (b < 0.0000001) return a;                // Since there is a limited precision we need to limit the value.
-
-    return this.gcd(b, Math.floor(a % b));           // Discard any fractions due to limitations in precision.
-  };
 
   gotoPrevLightboxImage() {
     const { photo } = this.state
@@ -64,11 +44,10 @@ class GalleryComposition extends Component {
 
   render() {
     return (
-      <>
+      <Layout>
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2}}>
           <Masonry gutter="5px">
             {this.state.photos_fluid.map((photo, i) => {
-              console.log("photo in map:", photo)
               return(
                 <a
                   key={i}
@@ -94,7 +73,7 @@ class GalleryComposition extends Component {
           onClickNext={() => this.gotoNextLightboxImage()}
           onClose={() => this.closeLightbox()}
         />
-      </>
+      </Layout>
     )
   }
 }
